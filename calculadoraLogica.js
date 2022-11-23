824ba93e7e88cd2780a53cddee3fab2d004edf9e
Felipe Coimbra <felipegabycoimbra@hotmail.com>
2022-11-23 20:21:04 -0300

    first commit

---
+++ calculadoraLogica.js
@@ -0,0 +1,22 @@
+function checkCharacters() {
+  const caracteresValidos = ["a", "b", "c", "->", "<->", "~", "^", "v"];
+
+  var flag = true;
+  for (let i = 0; i < expressao.length; i++) {
+    const checkChar = expressao[i];
+
+    if (!caracteresValidos.includes(checkChar)) {
+      flag = false;
+    }
+  }
+
+  if (flag) {
+    console.log("Tudo certo!");
+  } else {
+    console.log("Tem erro.");
+  }
+}
+
+var expressao = prompt("Digite aqui: ");
+
+checkCharacters(expressao)
