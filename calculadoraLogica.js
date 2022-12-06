33aa6bd1c5793917bb0a2e771cd8032e5f2fd64c
Felipe Coimbra <felipegabycoimbra@hotmail.com>
2022-12-06 13:23:16 -0300

    tratamento de parentesis

--- calculadoraLogica.js
+++ calculadoraLogica.js
@@ -1,4 +1,4 @@
-function validarCaracteres() {
+function validarCaracteres(expressao) {
   const caracteresValidos = ["a", "b", "c", "->", "<->", "~", "^", "v"];
 
   var flag = true;
@@ -17,6 +17,64 @@ function validarCaracteres() {
   }
 }
 
+function validarSintaxe(expressao) {
+  const letras = ["a", "b", "c"];
+  const operadores = ["~", "^", "v", "->", "<->"];
+
+  var expressaoAnalisar = expressao.split(" ");
+
+  var flag = true;
+  var contaParentesis = 0;
+
+
+  for (let i = 0; i < expressaoAnalisar.length; i++) {
+    const caracter = expressaoAnalisar[i];
+
+    //TRATAMENTO PARA NEGAÇÃO
+    if (caracter.length <= 2) {
+      if (caracter.charAt(0) == "~") {
+        var possivelLetra = caracter.charAt(1);
+
+        //NEGAÇÃO SÓ PODE SER USADO ANTES DE LETRA
+        if (!letras.includes(possivelLetra)) {
+          flag = false;
+        }
+
+        
+      }
+    } else {
+      flag = false;
+    }
+
+    //TRATAMENTO DE PARENTESES
+    if (caracter == ")"){
+      if (contaParentesis > 0){
+        contaParentesis--;
+      } else {
+        flag = false;
+      }
+    } else if(caracter == "("){
+      contaParentesis++;
+    }
+  }
+
+  //CHECA SE A QUANTIDADE DE PARENTESIS QUE ABRE É IGUAL AO QUE FECHA
+  if(contaParentesis != 0){
+    flag = false;
+  }
+
+  if (flag){
+    console.log("Tudo certo!");
+  } else {
+    console.log("Tem erro.");
+  }
+}
+
+function calcular(a, operacao, b){
+
+}
+
 var expressao = prompt("Digite aqui: ");
-validarCaracteres(expressao)
-console.log("teste do git");
+validarSintaxe(expressao)
+
+
